0e1481edd4334d3eb68afc9cb0053fa2f613b536
Mannan88 <kocharmanan88@gmail.com>
2025-08-05 10:20:35 +0000

    UI changes in student page

--- src/new_hacathon_frontend/src/components/studentPage/DegreeDisplay.jsx
+++ src/new_hacathon_frontend/src/components/studentPage/DegreeDisplay.jsx
@@ -1,15 +1,19 @@
 import * as React from "react";
 
-const degrees = ["Degree 1", "Degree 2", "Degree 3", "Degree 4"]
+const degrees = ["Degree 1", "Degree 2", "Degree 3", "Degree 4","Degree 5"]
 
 function DegreeDisplay() {
     return (
         <div className="degree-div">
-            <ul className="degree-list">
+            <h1>Degree List</h1>
+        <div className="degree-grid">
                 {degrees.map((degree, index) => (
-                    <li className="degree-individual" key={index}>{degree}</li>
+                    <div className="degree-individual" key={index}>
+                        <img src="/"></img>
+                        <p>{degree}</p></div>
                 ))}
-            </ul>
+           
+        </div>
         </div>
 
     )
