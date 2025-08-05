5c6a65eb377a9cfecab7044d4cc4f8961dc75996
Mannan88 <kocharmanan88@gmail.com>
2025-08-05 13:54:12 +0000

    added notifier component in student page

---
+++ src/new_hacathon_frontend/src/components/studentPage/Notifier.jsx
@@ -0,0 +1,37 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell, faEye, faShield } from '@fortawesome/free-solid-svg-icons';
+
+const Notifier = ({ isUserLoggedIn, isUserGrantedAccess }) => {
+  return (
+    <div className="notifier">
+      {/* Shield icon */}
+      {!isUserGrantedAccess && !isUserLoggedIn && (
+        <FontAwesomeIcon
+          className="faicon"
+          style={{ color: 'black', fontSize: '18px' }}
+          icon={faShield}
+        />
+      )}
+
+      {/* "Maa ki aakh" icon */}
+      {isUserGrantedAccess && (
+        <FontAwesomeIcon
+          className="faicon"
+          style={{ color: 'black', fontSize: '18px' }}
+          icon={faEye}
+        />
+      )}
+
+      {/* Bell icon */}
+      {isUserLoggedIn && (
+        <FontAwesomeIcon
+          className="faicon"
+          style={{ color: 'black', fontSize: '18px' }}
+          icon={faBell}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Notifier;
--- src/new_hacathon_frontend/src/pages/StudentPage.jsx
+++ src/new_hacathon_frontend/src/pages/StudentPage.jsx
@@ -1,30 +1,35 @@
 import * as React from "react";
 import DegreeDisplay from "../components/studentPage/DegreeDisplay";
+import Notifier from "../components/studentPage/Notifier";
 import "../styles/studentPage.scss"
+
 function StudentPage() {
     return (
         <div className="student-page">
+           <Notifier
+            isUserLoggedIn={false}
+            isUserGrantedAccess={false}/>
             <div className="student-info">
                 <img className="student-image" src="/"></img>
-                <div  className="student-info-text">
+                <div className="student-info-text">
                     <h1 className="student-name">Shubhuuuuuu</h1>
                     <p className="student-bio">The company itself is a very successful company. What repulses those who praise him, what will happen to the suffering of those who are present, will I make some opening to the wise? Having said that, the trouble with the great hatred of the offices itself will turn out to be beneficial to some!</p>
                 </div>
             </div>
-           
-           <div className="activity-summary-log">
-            {/* Academic summary */}
-            <div className="academic-summary"> 
-                <h3 className="subheading">Academic Summary</h3>
-            </div>
-            {/* Recent Activity */}
-            <div className="recent-activity">
-                <h3 className="subheading">Recent Activity</h3>
-            </div>
+
+            <div className="activity-summary-log">
+                {/* Academic summary */}
+                <div className="academic-summary">
+                    <h3 className="subheading">Academic Summary</h3>
+                </div>
+                {/* Recent Activity */}
+                <div className="recent-activity">
+                    <h3 className="subheading">Recent Activity</h3>
+                </div>
             </div>
             {/* Display degrees */}
             <div>
-                <DegreeDisplay/>
+                <DegreeDisplay />
             </div>
 
         </div>
